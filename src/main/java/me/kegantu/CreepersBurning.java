fce0faae1ee301ebf5157a9c6524ecb10cbd7bf3
Kegantu <kaluznijartem99@gmail.com>
2023-02-19 16:23:23 +0200

    Initial commit

---
+++ src/main/java/me/kegantu/CreepersBurning.java
@@ -0,0 +1,14 @@
+package me.kegantu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreepersBurning implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/kegantu/mixin/CreeperEntityMixin.java
@@ -0,0 +1,37 @@
+package me.kegantu.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.AvoidSunlightGoal;
+import net.minecraft.entity.ai.goal.EscapeSunlightGoal;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreeperEntity.class)
+public abstract class CreeperEntityMixin extends HostileEntity {
+
+	protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Inject(at = @At("HEAD"), method = "initGoals")
+	protected void initGoals(CallbackInfo info) {
+		this.goalSelector.add(2, new AvoidSunlightGoal(this));
+		this.goalSelector.add(3, new EscapeSunlightGoal(this, 1d));
+	}
+
+	@Override
+	public void tick() {
+		if (this.isAlive()){
+			boolean AffectedBySunLight = this.isAffectedByDaylight();
+			if (AffectedBySunLight){
+				this.setOnFireFor(6);
+			}
+		}
+		super.tick();
+	}
+}
